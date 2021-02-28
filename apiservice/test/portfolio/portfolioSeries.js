0b1899cdce01bf3d18ce4a53f68d36756bf0e603
ManiloxGT <60653867+ManelGonzalez-ops@users.noreply.github.com>
2021-02-28 18:27:16 +0100

    added entry point for mocha

---
+++ apiservice/test/portfolio/portfolioSeries.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
+
+//we buy monday.
+//lunes 2021-02-01
+//update users set portfolioInitial = "2021-02-01" where userId=37
+
+//buy wednesday
+//miercoles 2021-02-03
+
+//we buy friday
+//viernes 2021-02-05
+
+//we buy sunday
+//doming 2021-02-07
