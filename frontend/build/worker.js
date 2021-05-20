2b47c9d88ab55ec2f8dd462db4da82de8f01e400
ManiloxGT <60653867+ManelGonzalez-ops@users.noreply.github.com>
2021-05-20 11:11:48 +0200

    added log

---
+++ frontend/build/worker.js
@@ -0,0 +1,65 @@
+
+this.onmessage = e => {
+    const companiesPrices = e.data
+
+    //this.postMessage({ culo: e.data })
+    let portfolioHistoryByDate = {}
+    let portfolioHistoryByCompanies = {};
+    Array.prototype.forEach.call(companiesPrices, company => {
+        //we take the unique key which is the ticker
+        console.log(company, "webos")
+        const ticker = Object.keys(company)[0]
+        if (company[ticker].length > 0) {
+            console.log(company[ticker, "pooouto"])
+            company[ticker].forEach(register => {
+                const isDateFormated = register.date.split("").find(char => char === "T")
+                let date = isDateFormated ?
+                    register.date.split("T")[0]
+                    :
+                    register.date
+
+                portfolioHistoryByDate[date] = {
+                    ...portfolioHistoryByDate[date],
+                    //aqui podriamos poner close high y todos
+                    [ticker]: {
+                        close: register.close
+                    }
+                }
+            })
+        }
+    })
+    companiesPrices.forEach(({ ...company }) => {
+        const ticker = Object.keys(company)[0]
+        console.log(ticker, "weba")
+        const companyHistory = company[ticker].map(register => {
+            const date = register.date.split("T")[0]
+            return (
+                [convertHumanToUnixInit(date),
+                register["adjClose"],
+                register["adjHigh"],
+                register["adjLow"],
+                register["adjOpen"]])
+        })
+        portfolioHistoryByCompanies[ticker] = companyHistory
+    })
+
+    this.postMessage({ portfolioHistoryByDate, portfolioHistoryByCompanies })
+}
+
+const convertHumanToUnixInit = (date) => {
+    const actualDate = date.split("-")
+    const mongol = parseInt(actualDate[1]) - 1
+    const formatedDate = new Date(
+        actualDate[0],
+        mongol.toString(),
+        actualDate[2]
+    );
+    return formatedDate.getTime();
+}
+
+const isDateClean = (date) => {
+    if (date.includes("T")) {
+        return false
+    }
+    return true
+}
