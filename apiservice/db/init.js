567ff52d6496913ddf35bdcff1a623951da8754d
ManiloxGT <60653867+ManelGonzalez-ops@users.noreply.github.com>
2021-05-26 17:09:51 +0200

    fix

--- apiservice/db/init.js
+++ apiservice/db/init.js
@@ -1,13 +1,13 @@
 const {
-  createUserTable, addNewUser, findUser, addOperation, createOperationTable, getOperations, addCompanyInfo, getCompanyInfo, createCompanyInfo, createCompaniesJsonTable, getMostActives, storeMostActives, deletePreviousDateRecord, createPortfolioTable,
+  createUserTable, addNewUser, findUser, addOperation, createOperationTable, getOperations, addCompanyInfo, getCompanyInfo, createCompanyInfo, createCompaniesJsonTable, getMostActives, storeMostActives, deletePreviousDateRecord, createPortfolioTable,getMaxConections
 } = require("./services");
 const { createInterestTable } = require("./services/interestsService");
 const { createPostRegister, createPostStructure } = require("./services/PostServices");
 const { createUserDetails } = require("./services/UserService");
 
 
-const initDb = () => {
-  
+const initDb = async() => {
+  await getMaxConections()
   createUserTable((err) => {
     console.log("que concha1");
     if (err) {
