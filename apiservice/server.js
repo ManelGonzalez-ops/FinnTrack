aeaf398b32a22068bb10a8d94b3aa9ab56c0ca70
ManiloxGT <60653867+ManelGonzalez-ops@users.noreply.github.com>
2021-05-26 13:46:05 +0200

    fix

--- apiservice/server.js
+++ apiservice/server.js
@@ -3,20 +3,20 @@ const PORT = process.env.PORT || 8001;
 module.exports = { PORT }
 const cors = require("cors");
 const app = express();
-//app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Credentials", true); 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   res.header("Access-Control-Allow-Credentials", true); 
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 const bodyParser = require("body-parser");
 const fs = require("fs");
